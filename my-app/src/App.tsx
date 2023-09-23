bee463d8e1389d7b53b82620cf3179e0734b3c70
Руслан Омаров <ruslan.omarov773@gmail.com>
2023-09-23 11:47:21 +0500

    reworked on react

---
+++ my-app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { PokemonCard } from "./components/PokemonCard";
+
+function App() {
+  return <PokemonCard />;
+}
+
+export default App;
---
+++ my-app/src/components/PokemonCard/index.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+interface IPokemon {
+  name: string;
+  id: number;
+}
+
+interface IResult {
+  count: number;
+  next: string;
+  previous?: string;
+  results: IPokemon[];
+}
+
+export const PokemonCard: React.FC = () => {
+  const [pokemons, setPokemons] = React.useState<IPokemon[]>();
+  const [query, setQuery] = useState<string>("");
+  const getPokemons = async (limit: number = 100) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`;
+    const { data } = await axios.get<IResult>(url);
+    console.log(data);
+    setPokemons(data.results);
+    return data.results;
+  };
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  return (
+    <div>
+      <input
+        placeholder="Enter Pokemon Name"
+        onChange={(event) => setQuery(event.target.value)}
+      />
+      <ul>
+        {pokemons
+          ?.filter((pokemon) => {
+            if (query === "") {
+              return pokemon;
+            } else if (
+              pokemon.name.toLowerCase().includes(query.toLowerCase())
+            ) {
+              return pokemon;
+            }
+          })
+          .map((pokemon, i) => (
+            <li key={i}>{pokemon.name}</li>
+          ))}
+      </ul>
+    </div>
+  );
+};
