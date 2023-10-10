5c4aa83d3a076cacf9a2a100a05ed969445c4c4d
Сафиуллин Руфат Рунарович <stud0000229588@study.utmn.ru>
2023-10-10 21:19:00 +0500

    New: добавил поиск по всем покемонам

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,62 +8,74 @@ import PokemonCard from "../../components/PokemonCard";
 import PokemonList from "../../components/PokemonList";
 import { useAppDispatch } from "../../redux/store";
 import {
-  fetchPokemons,
-  selectPokemonData,
+    fetchPokemons,
+    selectPokemonData,
 } from "../../redux/slices/pokemonSlice";
-import { setQuery } from "../../redux/slices/querySlice";
+import { selectQuery, setQuery } from "../../redux/slices/querySlice";
 
 const Home: React.FC = () => {
-  const dispatch = useAppDispatch();
-  const { pokemons, status, choosenPokemon } = useSelector(selectPokemonData);
+    const dispatch = useAppDispatch();
+    const { pokemons, status, choosenPokemon } = useSelector(selectPokemonData);
+    const { query } = useSelector(selectQuery);
 
-  const [currentPage, setCurrentPage] = React.useState<number>(0);
-  const getPokemons = async (limit: number = 100) => {
-    dispatch(fetchPokemons({ limit }));
-  };
+    const [currentPage, setCurrentPage] = React.useState<number>(0);
+    const getPokemons = async (limit: number = 100) => {
+        dispatch(fetchPokemons({ limit }));
+    };
 
-  const onFightButtonClick = () => {
-    if (pokemons && choosenPokemon) {
-      const randomPokemon = Math.floor(Math.random() * pokemons.length);
-    } else {
-      alert("Сначала выберите покемона!");
-    }
-  };
+    const onFightButtonClick = () => {
+        if (pokemons && choosenPokemon) {
+            const randomPokemon = Math.floor(Math.random() * pokemons.length);
+        } else {
+            alert("Сначала выберите покемона!");
+        }
+    };
 
-  const onChangeInput = (query: string) => {
-    dispatch(setQuery(query));
-  };
+    const onChangeInput = (query: string) => {
+        dispatch(setQuery(query));
+    };
 
-  useEffect(() => {
-    getPokemons();
-  }, []);
+    useEffect(() => {
+        getPokemons();
+    }, []);
 
-  const onChangePage = (page: number) => {
-    setCurrentPage(page);
-  };
+    const onChangePage = (page: number) => {
+        setCurrentPage(page);
+    };
 
-  return (
-    <div className={styles.container}>
-      <button onClick={onFightButtonClick} className={styles.fight_button}>
-        Бой!
-      </button>
-      <input
-        className={styles.effect_1}
-        type="text"
-        placeholder="Enter Pokemon Name"
-        onChange={(event) => onChangeInput(event.target.value)}
-      />
-      <div className={styles.pokemons}>
-        {pokemons && (
-          <PokemonList
-            pokemons={pokemons}
-            currentPage={currentPage}
-            onChangePage={onChangePage}
-          />
-        )}
-      </div>
-    </div>
-  );
+    const foundPokemons = pokemons?.filter((pokemon: IPokemon) => {
+        if (query === "") {
+            return pokemon;
+        } else if (pokemon.name.toLowerCase().includes(query.toLowerCase())) {
+            return pokemon;
+        }
+    });
+
+    return (
+        <div className={styles.container}>
+            <button
+                onClick={onFightButtonClick}
+                className={styles.fight_button}
+            >
+                Бой!
+            </button>
+            <input
+                className={styles.effect_1}
+                type="text"
+                placeholder="Enter Pokemon Name"
+                onChange={(event) => onChangeInput(event.target.value)}
+            />
+            <div className={styles.pokemons}>
+                {foundPokemons && (
+                    <PokemonList
+                        pokemons={foundPokemons}
+                        currentPage={currentPage}
+                        onChangePage={onChangePage}
+                    />
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Home;
