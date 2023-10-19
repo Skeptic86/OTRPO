8510c069b7d96204c10a2484b36885bad777911b
Сафиуллин Руфат Рунарович <stud0000229588@study.utmn.ru>
2023-10-19 21:32:28 +0500

    Fix: надо было поменять api/ на /api, также добавил gitignore и претир

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 const app = express();
 
 app.use(express.json());
-app.use('api/', fightResultRouter);
+app.use('/api', fightResultRouter);
 
 async function startApp() {
   try {
